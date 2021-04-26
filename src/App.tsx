d0e6d672ea838e7c77b0e8791270eee4a6e3ba2b
bora-uyumazturk <bora.uyumazturk@gmail.com>
2021-04-26 03:15:42 -0400

    use z-rotation to fix orientation before going on screen

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,47 @@ import React from "react";
 
 import FlipCard from "./components/FlipCard";
 
-import { useTapToFlip } from "./lib/hooks";
+import { useTapToFlip, useFlipGroup } from "./lib/hooks";
+
+// function App() {
+//   const { angle, ref, onClick } = useTapToFlip({
+//     defaultAngle: { x: 0, y: 0 },
+//   });
+//
+//   return (
+//     <div className="flex flex-col justify-center items-center h-screen w-full bg-gray-200 p-10 space-y-20">
+//       <div className="relative h-48 w-64" ref={ref} onClick={onClick}>
+//         <FlipCard angle={angle}>
+//           <div>Front</div>
+//         </FlipCard>
+//         <FlipCard angle={{ x: angle.x, y: angle.y + 1 }}>
+//           <div>Back</div>
+//         </FlipCard>
+//       </div>
+//     </div>
+//   );
+// }
 
 function App() {
-  const { angle, ref, onClick } = useTapToFlip({
-    defaultAngle: { x: 0, y: 0 },
-  });
+  const { angles, ref, onClick } = useFlipGroup();
 
   return (
     <div className="flex flex-col justify-center items-center h-screen w-full bg-gray-200 p-10 space-y-20">
       <div className="relative h-48 w-64" ref={ref} onClick={onClick}>
-        <FlipCard angle={angle}>
-          <div>Front</div>
+        <FlipCard angle={angles.top}>
+          <div>Top</div>
         </FlipCard>
-        <FlipCard angle={{ x: angle.x, y: angle.y + 1 }}>
-          <div>Back</div>
+        <FlipCard angle={angles.right}>
+          <div>Right</div>
+        </FlipCard>
+        <FlipCard angle={angles.bottom}>
+          <div>Bottom</div>
+        </FlipCard>
+        <FlipCard angle={angles.left}>
+          <div>Left</div>
+        </FlipCard>
+        <FlipCard angle={angles.home}>
+          <div>Home</div>
         </FlipCard>
       </div>
     </div>
