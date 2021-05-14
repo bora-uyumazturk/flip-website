6ad2fcc1386b4e460e1e2f38a1f23448566bb33c
bora-uyumazturk <bora.uyumazturk@gmail.com>
2021-05-14 01:57:45 -0400

    add alt and suppress target=_blank warnings

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,22 +6,25 @@ const Footer = () => {
           className="flex items-center justify-center"
           href="mailto:bora.uyumazturk@gmail.com"
           target="_blank"
+          rel="noreferrer"
         >
-          <img src="/mail.svg" className="h-6" />
+          <img src="/mail.svg" className="h-6" alt="mail" />
         </a>
         <a
           className="flex items-center justify-center"
           href="https://github.com/bora-uyumazturk/flipsite"
           target="_blank"
+          rel="noreferrer"
         >
-          <img src="/github.svg" className="h-6" />
+          <img src="/github.svg" className="h-6" alt="github" />
         </a>
         <a
           className="flex items-center justify-center"
           href="https://twitter.com/notsleepingturk"
           target="_blank"
+          rel="noreferrer"
         >
-          <img src="/twitter.svg" className="h-6" />
+          <img src="/twitter.svg" className="h-6" alt="twitter" />
         </a>
       </div>
     </footer>
