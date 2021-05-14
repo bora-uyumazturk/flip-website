e089414b64b6fcae4f4cc7555810662b30639eb6
bora-uyumazturk <bora.uyumazturk@gmail.com>
2021-05-14 13:23:49 -0400

    move back to index.html

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,11 @@ import { isMobile, isSafari } from "react-device-detect";
 
 import BrowserView from "./components/BrowserView";
 import MobileView from "./components/MobileView";
-import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
-      <Header />
       {isMobile || isSafari ? <MobileView /> : <BrowserView />}
       <Footer />
     </>
